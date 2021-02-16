adf07067954af640d424e328557eb6b2f5e8bb95
unknown <kevycosta@hotmail.com>
2021-02-15 23:24:00 -0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import { Script } from "vm"
+
+function TestarNewFramework() {
+    // console.log('Massinha!!!');
+    return <div>Welcome to Costa Design</div>
+}
+
+
+
+export default TestarNewFramework
